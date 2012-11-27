e1ee853bcf808707854ff4afff70229df939c448
Per Larsson <per.larsson@gmail.com>
2012-11-27 21:09:56 +0100

    Added enumerable extensions and geo collection unit tests.

--- DotIGC/GeoPositionCollection.cs
+++ DotIGC/GeoPositionCollection.cs
@@ -2,20 +2,26 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using System.Linq;
     using System.Text;
     using DotIGC.Records;
     
-    public class GeoPositionCollection
+    public class GeoPositionCollection : Collection<GeoPosition>
     {
         public GeoPositionCollection(IEnumerable<Record> records)
         {
+            if (records.IsEmpty())
+                return;
+            
             var fixRecords = records.OfType<FixRecord>();
-            GeoPositions = fixRecords.Zip(fixRecords.Skip(1), CreateGeoPosition)
-                                     .ToList();
-        }
+            var first = fixRecords.FirstOrDefault();
 
-        public List<GeoPosition> GeoPositions { get; private set; }
+            if (null != first)
+                Add(new GeoPosition(first.TimeUTC, first.Latitude, first.Longitude, first.PressureAltitude, first.GnssAltitude, 0.0, 0.0));
+
+            fixRecords.Zip(fixRecords.Skip(1), CreateGeoPosition).ForEach(Add);
+        }
 
         GeoPosition CreateGeoPosition(FixRecord a, FixRecord b)
         {
@@ -24,6 +30,7 @@
             double speed = (distance / seconds) * 3.6;
 
             return new GeoPosition(b.TimeUTC, b.Longitude, b.Latitude, b.PressureAltitude, b.GnssAltitude, double.NaN, speed);
-        }
+        }      
     }
 }
+ 
