96ed7443bcbc9f9e83ac32d7b58d58ede050ece1
Ludovic Launer <ludovic.launer@atos.net>
2020-11-20 18:30:51 +0100

    Fix parsing of unknown three letters code Change DateTime parsing so that it also works when the locale is not year-month-day

--- DotIGC/HeaderRecordReader.cs
+++ DotIGC/HeaderRecordReader.cs
@@ -13,10 +13,20 @@
                 throw new ArgumentException("Wrong record type");
 
             var source = text[1] == 'F' ? DataSource.FlightRecorder : DataSource.Other;
-            var tlc = (ThreeLetterCode)Enum.Parse(typeof(ThreeLetterCode), text.Substring(2, 3));
             var values = text.Split(new[] { ':' });
             var value = values.Length > 1 ? values[1] : string.Empty;
-            return new HeaderRecord(source, tlc, text, value);                
+
+            // Parse the Three letters code
+            if (Enum.TryParse<ThreeLetterCode>(text.Substring(2, 3), out ThreeLetterCode tlc))
+            {
+
+                return new HeaderRecord(source, tlc, text, value);
+            }
+            else
+            {
+                // Unknown three letter code found
+                return new HeaderRecord(source, ThreeLetterCode.X, text, value);
+            }
         }
     }
 }
